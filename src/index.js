408f5e44b2fd7c4c9eb7d4f6df6a1b2c4d9daf7b
natashagoryanina <natashagoryanina@gmail.com>
2021-08-30 20:07:32 +0300

    dark light theme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import menu from './menu.json';
-console.log(menu);
+import menuCardTemplate from './templates/menu-card.hbs';
 
-import menuCards from './templates/menu-card.hbs';
-console.log(menuCards);
+const menuList = document.querySelector('ul.js-menu');
+const menuCardMarkup = createMenuCardsMarkup(menu);
+
+menuList.insertAdjacentHTML('beforeend', menuCardMarkup);
+
+function createMenuCardsMarkup(menu) {
+    return menu.map(menuCardTemplate).join('');
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitch = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
+
+console.log(body);
+
+themeSwitch.addEventListener('change', onBodyThemeChange);
+
+function onBodyThemeChange(e) {
+    if (themeSwitch.hasAttribute('checked')) {
+        themeSwitch.removeAttribute('checked');
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        return;
+    }
+    else {
+        themeSwitch.setAttribute('checked', true);
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        return;
+    }
+}
